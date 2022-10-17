d78a71dad05961798a22059a8c3c380273bbfe08
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-10-17 13:23:29 -0700

    a6 React.js sp22

---
+++ src/labs/a6/hello-world.js
@@ -0,0 +1,10 @@
+import Nav from "/Users/rainiezhao/2022/fall/webdev/tuiter-react-web-app/src/nav.js";
+function HelloWorld() {
+ return (
+   <div>
+     <Nav/>
+     <h1>Hello World!</h1>
+   </div>
+ );
+}
+export default HelloWorld;
