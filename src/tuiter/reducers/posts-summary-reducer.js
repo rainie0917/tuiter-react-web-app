1c0cea37db8abc8e67786735daf98c699a258d18
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-14 13:24:03 -0800

    remove post-summary-reducer

--- src/tuiter/reducers/posts-summary-reducer.js
+++
@@ -1,9 +0,0 @@
-import {createSlice} from "@reduxjs/toolkit";
-import postsSummary from '../data/tuits.json';
-
-const postsSummarySlice = createSlice({
-    name: 'postSummary',
-    initialState: postsSummary
-});
-
-export default postsSummarySlice.reducer;
